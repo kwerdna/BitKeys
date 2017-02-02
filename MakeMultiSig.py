37e2bee8d0dc7d53abf2c2860496bef6a3f3552a
andy k <andy@eborrower.com>
2017-02-02 02:09:04 -0800

    Added warnings to not use for real bitcoins MakeMultiSig.py

--- MakeMultiSig.py
+++ MakeMultiSig.py
@@ -6,6 +6,10 @@ import base58
 # usage: python MakeMultiSig.py min max
 # defaults to 2 of 3. maximum 16 of 16 
 
+# THIS IS FOR EDUCATIONAL PURPOSES ONLY!!!!
+# lacks security for the private keys 
+# you'd want to use bip32 
+
 minsigs = 2
 maxsigs = 3
 if len(sys.argv) == 3:
@@ -32,6 +36,7 @@ keypairs = returnkeys(maxsigs)
 # make the multi sig script and address
 print("Making {} of {} multisig address".format(minsigs, maxsigs))
 multisig_redeem_script = mk_multisig_script([p['compressed']['pub'] for p in keypairs], minsigs, maxsigs)
+
 multisig_address = scriptaddr(multisig_redeem_script) # main net
 testnet_multisig_address = scriptaddr(multisig_redeem_script, 196) # testnet
 
@@ -42,4 +47,5 @@ print("redeem: {}\n".format(multisig_redeem_script))
 #for k in keypairs:
 for i in range(len(keypairs)):
 	print "prv{}: {}".format(i+1, keypairs[i]['priv'])
+	print "pub{}: {}".format(i+1, keypairs[i]['compressed']['pub'])
 	print "wif{}: {}".format(i+1, keypairs[i]['compressed']['wif'])
